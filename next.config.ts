b24d4a37dce076e7f6c8e4d891ff589c4fdfc75c
Samandar Turg'unboev <turgunboyevsamandar4@gmail.com>
2025-05-15 15:53:56 +0500

    true

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
   eslint: {
-    ignoreDuringBuilds: false
+    ignoreDuringBuilds: true
   },
   images: {
     remotePatterns: [
