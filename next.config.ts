2b01386980400e6570396e30b84728b71944b477
Samandar Turg'unboev <turgunboyevsamandar4@gmail.com>
2025-05-15 15:47:31 +0500

    eslint false

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: false
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "github.com", pathname: "**" },
